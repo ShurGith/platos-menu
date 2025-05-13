e4046060d2a595a1679d16c778d5387cd3c53ebc
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-05-13 10:15:11 +0200

    modal

--- src/compontes/CardsContainer.jsx
+++ src/compontes/CardsContainer.jsx
@@ -1,5 +1,6 @@
 import Card from "./Card";
 import Ordered from "./Ordered";
+import ModalOrder from "./ModalOrder";
 import { useOrderContext } from "../context/OrderContext";
 import { useProduct } from "../context/ProductoContext";
 function CardsContainer() {
@@ -7,24 +8,27 @@ function CardsContainer() {
     const { data } = useProduct();
 
     return (
-        <div className="w-full flex flex-col lg:flex-row gap-4 px-2 lg:px-18 py-6">
-            <div className="">
-                <h1 className="text-4xl font-siete text-rosado-90 lg:pt-8 pb-4 ">Desserts</h1>
-                <div className="lg:grid lg:grid-cols-3 lg:gap-4 w-full">
-                    {data && data.map((item, index) => (
-                        <Card numberId={index}
-                            key={item.name}
-                            item={item} />
-                    ))}
+        <>
+            <ModalOrder />
+            <div className="w-full flex flex-col lg:flex-row gap-4 px-2 lg:px-18 py-6 relative overflow-hidden">
+                <div className="">
+                    <h1 className="text-4xl font-siete text-rosado-90 lg:pt-8 pb-4 ">Desserts</h1>
+                    <div className="lg:grid lg:grid-cols-3 lg:gap-4 w-full">
+                        {data && data.map((item, index) => (
+                            <Card numberId={index}
+                                key={item.name}
+                                item={item} />
+                        ))}
+                    </div>
+                </div>
+                <div className="lg:min-w-1/4  bg-white h-fit mt-4 rounded-xl py-4 ">
+                    <h2 className="text-xl text-center text-rojo font-siete mb-4 ">
+                        Your Cart <span className="font-siete"> ({counter})</span>
+                    </h2>
+                    <Ordered />
                 </div>
             </div>
-            <div className="lg:min-w-1/4  bg-white h-fit mt-4 rounded-xl py-4 ">
-                <h2 className="text-xl text-center text-rojo font-siete mb-4 ">
-                    Your Cart <span className="font-siete"> ({counter})</span>
-                </h2>
-                <Ordered />
-            </div>
-        </div>
+        </>
     )
 }
 
--- src/compontes/ModalOrder.jsx
+++ src/compontes/ModalOrder.jsx
@@ -6,15 +6,21 @@ function ModalOrder() {
     const totalPay = data.reduce((acc, item) => acc + Number(item.total), 0).toFixed(2);
 
     return (
-        <div className="lg:min-w-1/4  bg-white h-fit mt-4 rounded-xl py-4 " >
-            <h2 className="text-xl text-center text-rojo font-siete mb-4 ">
-                Your Cart <span className="font-siete"> ({counter})</span>
+        <div className="w-screen h-screen  bg-black/60 fixed z-1 ">
+        <div className="lg:w-2/6 bg-white h-fit mt-4 rounded-xl py-4 " >
+        <div className="flex flex-col px-5 py-6 gap-4">
+            <img className="size-10" src='/assets/images/icon-order-confirmed.svg' alt="" />
+            <h2 className="text-2xl text-rosado-90 font-siete ">
+               Order Confirmed
             </h2>
-            <div className="flex flex-col gap-4 w-full text-main lg:px-6">
+            <p className="text-sm text-rosado-90/50">We hope you enjoy your food</p>
+            </div>
+            <div className="flex flex-col px-5  py-6 gap-4 w-full text-main">
                 {data &&
-                    <div className="flex flex-col gap-4 w-full">
+                    <div className="flex flex-col bg-rosado-10 gap-6 w-full px-6 py-6">
                         {data.map((item) => (
                             <div key={item.name} className="flex w-full">
+                            <img src={item.image} alt="" />
                                 <div className="flex flex-col w-full">
                                     <p className="font-cinco ml-6">{item.name}</p>
                                     <div className="flex items-center justify-between text-rosado-40 ">
@@ -31,9 +37,10 @@ function ModalOrder() {
                             <h4 className="ont-cuatro text-sm">Order total</h4>
                             <h4 className="font-siete text-2xl text-center"> ${totalPay}</h4>
                         </div>
-                        <button className="cursor-pointer bg-rojo text-rosado-10 py-2 rounded-full">Confirm Order</button>
                     </div>
                 }
+                        <button className="cursor-pointer bg-rojo text-rosado-10 py-2 rounded-full">Confirm Order</button>
+            </div>
             </div>
         </div>
     )
