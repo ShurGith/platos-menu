fa65cceb1c919bc7c6faeaeaa8d1980b8ef4a28e
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-05-15 09:56:39 +0200

    Update CardsContainer.jsx

--- src/compontes/CardsContainer.jsx
+++ src/compontes/CardsContainer.jsx
@@ -10,47 +10,43 @@ function CardsContainer() {
     const { counter, tableActual } = useCalcsContext();
     const { data } = useProduct();
 
-    return (
-        <>
-            <ModalOrder />
+    return <>
+        <ModalOrder />
 
-            <div className="w-full flex flex-col lg:flex-row gap-4 px-2 lg:px-18 py-6 relative overflow-hidden">
+        <div className="w-full  flex flex-col flex-col-reverse  lg:grid lg:grid-cols-6 gap-2 px-2 lg:px-18 py-6 relative overflow-hidden">
 
-                <div className="">
-                    <h1 className="text-4xl font-siete text-rosado-90 lg:pt-8 pb-4 ">Desserts</h1>
-                    <div className="lg:grid lg:grid-cols-3 lg:gap-4 w-full">
-                        {data && data.map((item, index) => (
-                            <Card numberId={index+1}
-                                key={index}
-                                item={item} />
-                        ))}
-                    </div>
-                </div>
-
-                <div className="lg:min-w-1/4 mt-8 h-fit gap-4 flex flex-col">
-                    <div className="flex flex-col gap-4 lg:px-6 px-2 py-4">
-                        <TablesSelect />
-                    </div>
-                    <div className="lg:min-w-1/4  bg-white h-fit rounded-xl py-4 ">
-                        {tableActual && counter > 0 &&
-                            <>
-                                <h2 className="text-xl border-b border-rosado-10 ml-6 text-rojo font-siete mb14 ">
-                                    Table #: {tableActual}
-                                    <span className="text-base font-cinco ml-2">Order Summary</span>
-                                </h2>
-
-                                <h3 className="ml-18 mt-2 mb-2 text-rojo text-cinco">
-                                    Total quantity <span className="font-siete">
-                                        ({counter})</span></h3>
-                            </>
-                        }
-
-                        <Ordered />
-                    </div>
+            <div className="lg:col-span-4">
+                <h1 className="text-4xl font-siete text-rosado-90 lg:pt-8 pb-4 ">Desserts</h1>
+                <div className="lg:grid lg:grid-cols-3 w-full">
+                    {data && data.map((item, index) => (
+                        <Card numberId={index+1} key={index} item={item} />
+                    ))}
                 </div>
             </div>
-        </>
-    )
+
+            <div className="lg:col-span-2 mt-8 h-fit gap-4 flex flex-col">
+                <div className="flex flex-col gap-4 lg:px-6 px-2 py-4">
+                    <TablesSelect />
+                </div>
+                <div className="lg:min-w-1/4  bg-white h-fit rounded-xl py-4 ">
+                    {tableActual && counter > 0 &&
+                        <>
+                            <h2 className="text-xl border-b border-rosado-10 ml-6 text-rojo font-siete mb14 ">
+                                Table #: {tableActual}
+                                <span className="text-base font-cinco ml-2">Order Summary</span>
+                            </h2>
+
+                            <h3 className="ml-18 mt-2 mb-2 text-rojo text-cinco">
+                                Total quantity <span className="font-siete">
+                                    ({counter})</span></h3>
+                        </>
+                    }
+
+                    <Ordered />
+                </div>
+            </div>
+        </div>
+    </>;
 }
 
 export default CardsContainer
